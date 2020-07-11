de70d0c9f3334c4945bf7587c729121fecc48d61
krits29 <31025768+krits29@users.noreply.github.com>
2020-07-11 14:21:39 -0700

    styling Person.css

--- the-basics/src/App.js
+++ the-basics/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
@@ -6,31 +6,95 @@ class App extends Component {
   state = {
     people: [
       {name: "Mickey", animal: "Mouse"},
-      {name: "Donald", animal: "Duck"}
+      {name: "Donald", animal: "Duck"},
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log("Clicked!");
     this.setState({
       people: [
-        {name: "Minnie", animal: "Mouse"},
+        {name: newName, animal: "Mouse"},
         {name: "Donald", animal: "Duck"}
       ]
     })
   }
-  
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      people: [
+        {name: "Mickey", animal: "Mouse"},
+        {name: event.target.value, animal: "Duck"}
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "2px solid gray",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>Basics of React</h1>
         <p>We need to understand this jsx, and don't get confused with html</p>
-        <button onClick = {this.switchNameHandler}>Switch name</button>
-        <Person name = {this.state.people[0].name} animal = {this.state.people[0].animal}/>
-        <Person name = {this.state.people[1].name} animal = {this.state.people[0].animal}>"He he he"</Person>
+        <button 
+          style = {style} //inline styling
+          onClick = {() => this.switchNameHandler()}>Switch name</button>
+        <Person 
+          name = {this.state.people[0].name} 
+          click = {this.switchNameHandler.bind(this, "Minnie")}
+          animal = {this.state.people[0].animal}/>
+        <Person 
+          name = {this.state.people[1].name} 
+          animal = {this.state.people[1].animal}
+          changed = {this.nameChangedHandler}/>
       </div>
     );
   }
 }
 
 export default App; 
+
+
+
+//hooks
+//import React, {useState} from 'react';
+
+//same thing here:
+  
+// const app = (props) => {
+//   const [ peopleState, setPeopleState] = useState({
+//     people: [
+//       {name: "Mickey", animal: "Mouse"},
+//       {name: "Donald", animal: "Duck"}
+//     ]
+//   });//returning two elements: current state & a function which allows us to update it
+
+//   const switchNameHandler = () => {
+//     //console.log("Clicked!");
+//     setPeopleState({
+//       people: [
+//         {name: "Minnie", animal: "Mouse"},
+//         {name: "Donald", animal: "Duck"}
+//       ]
+//     })
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>Basics of React</h1>
+//       <p>We need to understand this jsx, and don't get confused with html</p>
+//       <button onClick = {switchNameHandler}>Switch name</button>
+//       <Person name = {peopleState.people[0].name} animal = {peopleState.people[0].animal}/>
+//       <Person name = {peopleState.people[1].name} animal = {peopleState.people[0].animal}>"He he he"</Person>
+//     </div>
+//   );
+  
+// }
+
+// export default app; 
--- the-basics/src/Person/Person.js
+++ the-basics/src/Person/Person.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
-            <p>----person-----</p>
+        <div className = "Person">
             <p>Adding in custom js code with curly braces</p>
             <p>Using random number: {Math.floor(Math.random() * 50)}</p>
-            <p>Given name: {props.name}</p>
+            <p onClick = {props.click}>Given name: {props.name}</p>
             <p>Given animal: {props.animal}</p>
             <p>{props.children}</p>
+            <input type = "text" onChange = {props.changed} value = {props.name}/>
         </div>
     );
 };
